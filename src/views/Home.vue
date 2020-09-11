1b855c539c22ced5b36f06f5c5b5f9de11709774
chenzihao <278199196@qq.com>
2020-09-11 23:26:04 +0800

    chapter 23-1:移动报表项目数据接口请求+loading效果

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="home">
+  <div
+    class="home"
+    v-if="!loading"
+  >
     <div class="datav-wrapper">
       <top-header></top-header>
       <sales-bar></sales-bar>
@@ -9,6 +12,12 @@
       <sale-sun></sale-sun>
       <sales-radar></sales-radar>
     </div>
+  </div>
+  <div
+    class="home"
+    v-else
+  >
+    <div class="loading-wrapper">{{loadingText}}</div>
   </div>
 </template>
 
@@ -20,6 +29,7 @@ import SalePie from '@/components/SalePie'
 import SaleMap from '@/components/SaleMap'
 import SaleSun from '@/components/SalesSun'
 import SalesRadar from '@/components/SalesRadar'
+import { getScreenMobileData } from '@/api/index.js'
 
 export default {
   name: 'Home',
@@ -34,9 +44,29 @@ export default {
     SalesRadar
   },
 
-  data () {
+  data() {
     return {
+      loading: true,
+      loadingText: '加载中.',
+      data: null
     }
+  },
+
+  mounted() {
+    this.task && clearInterval(this.task)
+    this.task = setInterval(() => {
+      if (this.loadingText === '加载中...') {
+        this.loadingText = '加载中.'
+      } else {
+        this.loadingText += '.'
+      }
+    }, 200)
+
+    getScreenMobileData().then(res => {
+      this.loading = false
+      this.task && clearInterval(this.task)
+      this.data = res.data
+    })
   }
 
 }
@@ -46,6 +76,7 @@ export default {
 .home {
   position: relative;
   height: 100%;
+  width: 100%;
   .datav-wrapper {
     position: absolute;
     top: 0;
@@ -56,6 +87,16 @@ export default {
     background-image: url('//www.youbaobao.xyz/datav-res/datav/datav-mobile-bg.jpg');
     background-size: 100% 100%;
     background-repeat: no-repeat;
+  }
+
+  .loading-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 40px;
+    color: #fff;
   }
 }
 </style>
